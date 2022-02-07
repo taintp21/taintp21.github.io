81472e854cbae810ba8c43ba543d58b29b1420ca
Tai Nguyen <98073794+taintp21@users.noreply.github.com>
2022-02-07 11:43:13 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/su-kien', [HomeController::class, 'sukien']);
 Route::get('/lien-he', [HomeController::class, 'lienhe']);
+Route::post('/send-email', [HomeController::class, 'sendEmail'])->name('send.email');
