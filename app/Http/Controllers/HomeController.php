9ec8d88b13a9a6014cf92cbc418259c3992f3fa0
Tai Nguyen <98073794+taintp21@users.noreply.github.com>
2022-02-07 11:44:32 +0700

    Changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
@@ -14,5 +16,16 @@ class HomeController extends Controller
     }
     public function lienhe(){
         return view('lienhe');
+    }
+    public function sendEmail(Request $req){
+        $data = [
+            'name' => $req->name,
+            'email' => $req->email,
+            'phone' => $req->phone,
+            'address' => $req->address,
+            'message' => $req->message
+        ];
+        Mail::to('taintp21@gmail.com')->send(new ContactMail($data));
+        return redirect()->back()->with('message', 'Gửi liên hệ thành công.<br> Vui lòng kiên nhẫn đợi phản hồi từ chúng tôi, bạn nhé!');
     }
 }
