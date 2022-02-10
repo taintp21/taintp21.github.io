6ea1b4356a90d56a2e0044be77f98f11683f7090
Tai Nguyen <98073794+taintp21@users.noreply.github.com>
2022-02-10 11:16:11 +0700

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,7 +1,8 @@
 <p>
     <b>Tên:</b> {{$data['name']}} <br>
     <b>Email:</b> {{$data['email']}} <br>
-    <b>Điện thoại:</b> {{$data['phone']}} <br><br>
+    <b>Điện thoại:</b> {{$data['phone']}} <br>
+    <b>Địa chỉ:</b> {{$data['address']}} <br><br>
     <b>Tin nhắn:</b> <br>
     <span>{{$data['message']}}</span>
 </p>
