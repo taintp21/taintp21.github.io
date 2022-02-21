af0ff9ee9184a79bd71ec3e26b5ed4aa1c219770
Tai Nguyen <98073794+taintp21@users.noreply.github.com>
2022-02-21 13:41:53 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/form-thanhtoan', [HomeController::class, 'formThanhToan']);
 Route::get('/su-kien', [HomeController::class, 'sukien']);
 Route::get('/su-kien/{id}', [HomeController::class, 'chitiet_sukien']); //Xem chi tiết
 Route::get('/lien-he', [HomeController::class, 'lienhe']);
@@ -12,4 +13,4 @@ Route::get('/lien-he', [HomeController::class, 'lienhe']);
 
 Route::post('/send-email', [HomeController::class, 'sendEmail'])->name('send.email');
 
-
+Route::post('/form-thanhtoan/thanhtoan',[HomeController::class, 'thanhtoan']);
